cc41d31d21fdf28490fe8d694fa9c3266e12b9c3
Robrecht Cannoodt <rcannood@gmail.com>
2023-06-28 22:26:05 +0200

    update enums

--- testmap/process.R
+++ testmap/process.R
@@ -26,48 +26,81 @@ library(googlesheets4)
 gs4_auth("robrecht.cannoodt@gmail.com")
 
 terrains <- read_sheet("1bOlgW25zpWOUTPcPcNDbfpXK5f90J2BBaBuzwypABQs", "terrains")
-terrains$terrain_i <- map$terrain[terrains$testmap_tile + 1]
+terrains$terrain_int32le <- map$terrain[terrains$testmap_tile + 1]
 write_sheet(terrains, "1bOlgW25zpWOUTPcPcNDbfpXK5f90J2BBaBuzwypABQs", "terrains")
 
 features <- read_sheet("1bOlgW25zpWOUTPcPcNDbfpXK5f90J2BBaBuzwypABQs", "features")
-features$feature_i <- map$feature[features$testmap_tile + 1]
+features$feature_int32le <- map$feature[features$testmap_tile + 1]
 write_sheet(features, "1bOlgW25zpWOUTPcPcNDbfpXK5f90J2BBaBuzwypABQs", "features")
 
 improvements <- read_sheet("1bOlgW25zpWOUTPcPcNDbfpXK5f90J2BBaBuzwypABQs", "improvements")
-improvements$improvement_i <- map$improvement[improvements$testmap_tile + 1]
+improvements$improvement_int32le <- map$improvement[improvements$testmap_tile + 1]
 write_sheet(improvements, "1bOlgW25zpWOUTPcPcNDbfpXK5f90J2BBaBuzwypABQs", "improvements")
 
 continents <- read_sheet("1bOlgW25zpWOUTPcPcNDbfpXK5f90J2BBaBuzwypABQs", "continents")
-continents$continent_i <- map$continent[continents$testmap_tile + 1]
+continents$continent_int32le <- map$continent[continents$testmap_tile + 1]
 write_sheet(continents, "1bOlgW25zpWOUTPcPcNDbfpXK5f90J2BBaBuzwypABQs", "continents")
 
 resources <- read_sheet("1bOlgW25zpWOUTPcPcNDbfpXK5f90J2BBaBuzwypABQs", "resources")
-resources$resource_i <- map$resource[resources$testmap_tile + 1]
+resources$resource_int32le <- map$resource[resources$testmap_tile + 1]
 write_sheet(resources, "1bOlgW25zpWOUTPcPcNDbfpXK5f90J2BBaBuzwypABQs", "resources")
 
+roads <- read_sheet("1bOlgW25zpWOUTPcPcNDbfpXK5f90J2BBaBuzwypABQs", "roads")
+roads$road_int32le <- map$road[roads$testmap_tile + 1]
+write_sheet(roads, "1bOlgW25zpWOUTPcPcNDbfpXK5f90J2BBaBuzwypABQs", "roads")
+
+
 # check rivers
-map[c(665, 666, 741, 742)+1,] %>% as.data.frame
+map[c(665, 666, 741, 742, 2748, 2750, 2752)+1,] %>% as.data.frame
 
 # check cliffs
-map[c(665, 668, 670, 671)+1,] %>% as.data.frame
+map[c(665, 668, 670, 671, 2682, 2684, 2686)+1,] %>% as.data.frame
 
-# check pillaged improvements
-map[c(665, 1407, 1408, 1410)+1,] %>% as.data.frame
+# check non-pillaged vs pillaged improvements
+map[c(1481, 1482, 1484, 1407, 1408, 1410)+1,] %>% as.data.frame
 
-buffer_tiles <- map %>%
-  mutate(
-    i = row_number() - 1,
-    x = i %% width,
-    y = floor(i / width),
-  ) %>%
-  filter(!map_lgl(buffer0, is.null)) %>%
-  select(i, x, y, terrain, feature, resource, buffer0, tile_overlay_num) %>%
-  mutate(
-    buffer0_len = map_int(buffer0, length),
-    buffer0 = map_chr(buffer0, paste, collapse = "")
-  )
+# check feature rotations
+map[c(2295:2301)+1,] %>% as.data.frame
 
-buffer_tiles %>% print(n = 300)
+# coastal lowland
+map[c(2306:2309)+1,] %>% as.data.frame
+
+# units
+map[c(2812:2816)+1,] %>% as.data.frame
+
+# resource counts
+map[c(963:966)+1,] %>% as.data.frame
+
+# districts
+map[c(2004:2007, 2079:2081, 2153)+1,] %>% as.data.frame
+
+
+# generate enums
+terrains <- read_sheet("1bOlgW25zpWOUTPcPcNDbfpXK5f90J2BBaBuzwypABQs", "terrains")
+features <- read_sheet("1bOlgW25zpWOUTPcPcNDbfpXK5f90J2BBaBuzwypABQs", "features")
+improvements <- read_sheet("1bOlgW25zpWOUTPcPcNDbfpXK5f90J2BBaBuzwypABQs", "improvements")
+continents <- read_sheet("1bOlgW25zpWOUTPcPcNDbfpXK5f90J2BBaBuzwypABQs", "continents")
+resources <- read_sheet("1bOlgW25zpWOUTPcPcNDbfpXK5f90J2BBaBuzwypABQs", "resources")
+roads <- read_sheet("1bOlgW25zpWOUTPcPcNDbfpXK5f90J2BBaBuzwypABQs", "roads")
+
+
+fun <- function(df, nameenum, namecol, hexcol) {
+  names <- gsub("[^a-zA-Z]", "", stringi::stri_trans_general(df[[namecol]], id = "Latin-ASCII"))
+  hexes <- map_chr(str_extract_all(df[[hexcol]], ".."), function(x) paste(rev(x), collapse = ""))
+  codes <- paste(paste0("    ", names, " = 0x", hexes), collapse = ",\n")
+
+  paste0("enum ", nameenum, " : s32 {\n", codes, "\n};\n")
+}
+cat(
+  fun(terrains, "Terrain", "terrain_name", "terrain_hex"),
+  fun(features, "Feature", "feature_name", "feature_hex"),
+  fun(improvements, "Improvement", "improvement_name", "improvement_hex"),
+  fun(continents, "Continent", "continent_name", "continent_hex"),
+  fun(resources, "Resource", "resource_name", "resource_hex"),
+  sep = "\n"
+)
+
+
 
 # width <- 74
 # df <- map %>%
