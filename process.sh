22305f7c4199c8105c15a67c77cd85c562ab0d43
Robrecht Cannoodt <rcannood@gmail.com>
2023-07-03 21:41:35 +0200

    split up processing script

--- process.sh
+++ process.sh
@@ -7,11 +7,16 @@ for civ6save in $(find ./ -name '*.Civ6Save'); do
   output_prefix="${civ6save//\.Civ6Save/}"
 
   if [ ! -f "${output_prefix}.bin" ]; then
-    echo "processing '$civ6save'"
+    echo "Generating bin for '$civ6save'"
 
     viash run ../civ6_pipeline/src/civ6_save_renderer/dump_decompressed/config.vsh.yaml -- \
       --input ${output_prefix}.Civ6Save \
       --output ${output_prefix}.bin
+  
+  fi
+
+  if [ ! -f "${output_prefix}.json" ]; then
+    echo "Generating json for '$civ6save'"
 
     viash run ../civ6_pipeline/src/civ6_save_renderer/parse_imhex/config.vsh.yaml -- \
       --input ${output_prefix}.bin \
@@ -19,6 +24,5 @@ for civ6save in $(find ./ -name '*.Civ6Save'); do
       --pattern ../civ6save_analysis/main.hex \
       --includes ../civ6save_analysis/patterns/ \
       --include_imhex_patterns
-  
   fi
 done
